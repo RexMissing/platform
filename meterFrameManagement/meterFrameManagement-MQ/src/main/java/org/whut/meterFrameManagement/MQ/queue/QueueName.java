08c8277237ed0aab8f268c4d993264d041dbcfc7
RexMissing <916736852@qq.com>
2017-07-27 12:21:36 +0800

    07/27/12:21

--- meterFrameManagement/meterFrameManagement-MQ/src/main/java/org/whut/meterFrameManagement/MQ/queue/QueueName.java
+++
@@ -1,9 +0,0 @@
-package org.whut.meterFrameManagement.MQ.queue;
-
-/**
- * Created by zhang_minzhong on 2017/3/2.
- */
-public class QueueName {
-    public static final String sendQueue = "meter.json.sendQueue";
-    public static final String receiveQueue = "meter.byte.receiveQueue";
-}
